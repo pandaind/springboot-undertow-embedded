1899d129d03508c3fcf04ea948e9035ae120973d
Chittaranjan Panda <chittaranjan@hotmail.com>
2022-02-10 19:06:03 +0530

    recommit repo

---
+++ src/test/java/com/github/udaharan/springbootundertowembedded/SpringbootUndertowEmbeddedApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.udaharan.springbootundertowembedded;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootUndertowEmbeddedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
